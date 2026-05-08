fcb969011090b3f48c6b29de0a409cf104b81cb9
jm underwood <jm.underwood@hotmail.com>
2026-05-07 23:13:01 -0400

    Commented out the test code for checking if the exceptions for each of the classes in the main function so I could test the Select Screen portion without dealing with previous output.
    I also commented out cout statements in the ProductionWorker constructor and the printProductionWorker function

--- Factory Worker with Encapsulted Exceptions1/Factory Worker Heirarchy Project.cpp
+++ Factory Worker with Encapsulted Exceptions1/Factory Worker Heirarchy Project.cpp
@@ -12,10 +12,13 @@
 int main()
 {
 	bool tryagain = true;
+	int e_num;
+	int Prod_shift;
+	int Prod_pr;
 	
 	//****** EMPLOYEE EXCEPTION TEST ******//
 
-	cout << endl << "///// EMPLOYEE EXCEPTION TEST /////" << endl << endl;
+	/*cout << endl << "///// EMPLOYEE EXCEPTION TEST /////" << endl << endl;
 
 	Employee empTest;
 	int e_num;
@@ -39,12 +42,12 @@ int main()
 		}
 	}
 
-	cout << "The Employee Number is: " << empTest.getEmpNum() << endl;
+	cout << "The Employee Number is: " << empTest.getEmpNum() << endl;*/
 
 
 	//****** PRODUCTION WORKER EXCEPTION TEST ******//
 
-	cout << endl << "///// PRODUCTION WORKER EXCEPTION TEST /////" << endl << endl;
+	/*cout << endl << "///// PRODUCTION WORKER EXCEPTION TEST /////" << endl << endl;
 
 	
 	ProductionWorker ProdTest; // your code here
@@ -96,9 +99,105 @@ int main()
 		}
 	}
 
-	cout << "The Production Worker Pay Rate is: " << ProdTest.getHourlyPay() << endl << endl;
+	cout << "The Production Worker Pay Rate is: " << ProdTest.getHourlyPay() << endl << endl;*/
 
 
+	//****** SELECT SCREEN EXCEPTION TEST ******//
 
+	cout << endl << "///// SELECT SCREEN EXCEPTION TEST /////" << endl << endl;
+
+	bool choicetrue = true;
+	int emp_choice;
+
+	while (true)
+	{
+		cout << "// SELECT AN EMPLOYEE TYPE" << endl
+			<< "1. Produciton Worker" << endl
+			<< "2. Shift Supervisor" << endl
+			<< "3. Team Leader" << endl
+			<< "4. Exit" << endl;
+
+		cin >> emp_choice;
+
+		
+		cout << "Enter Employee Number: ";
+		cin >> e_num;
+
+		//Employee 
+
+		while (tryagain)
+		{
+			try
+			{
+				Employee temp;
+
+				temp.setEmpNum(e_num);
+
+				tryagain = false;
+			}
+
+			catch (Employee::InvalidEmployeeNumber)
+			{
+				cout << "Please enter a value between 0 and 9999: ";
+				cin >> e_num;
+			}
+		}
+
+
+		if (emp_choice == 1)
+		{
+			ProductionWorker* pw = new ProductionWorker();
+
+			pw->setEmpNum(e_num);
+
+			cout << "Enter Shift: ";
+			cin >> Prod_shift;
+
+			//tryagain = true;
+
+			while (tryagain)
+			{
+				try
+				{
+					pw->setShift(Prod_shift);
+
+					tryagain = false;
+				}
+
+				catch (ProductionWorker::InvalidShift)
+				{
+					cout << "Please enter either 1 or 2: ";
+					cin >> Prod_shift;
+				}
+			}
+
+			//cout << "The Production Worker shift is: " << ProdTest.getShift() << endl << endl;
+
+			cout << "Enter Pay Rate: ";
+			cin >> Prod_pr;
+
+			tryagain = true;
+			while (tryagain)
+			{
+				//cout << "While loop entered";
+				try
+				{
+					pw->setPayRate(Prod_pr);
+
+					tryagain = false;
+				}
+
+				catch (ProductionWorker::InvalidPayrate)
+				{
+					cout << "Please enter a non-negative value: ";
+					cin >> Prod_pr;
+				}
+			}
+
+			pw->printProdWorkInfo();
+		}
+
+
+	}
 
 }
--- Factory Worker with Encapsulted Exceptions1/ProductionWorker.cpp
+++ Factory Worker with Encapsulted Exceptions1/ProductionWorker.cpp
@@ -8,7 +8,7 @@ using namespace std;
 ProductionWorker::ProductionWorker(string e_name, int e_num, string H_date, int s, double h_pay)
 	: Employee(e_name, e_num, H_date), shift(s), hourly_pay(h_pay) 
 {
-	cout << "Constructor starting" << endl;
+	//cout << "Constructor starting" << endl;
 
 	//setShift(s);
 
@@ -44,7 +44,7 @@ void ProductionWorker::printProdWorkInfo() const
 {
 	printEmpInfo();
 
-	cout << "printProdWorkInfo is called" << endl << endl;
+	//cout << "printProdWorkInfo is called" << endl << endl;
 
 	cout << left << setw(18) << "Shift: ";
 
